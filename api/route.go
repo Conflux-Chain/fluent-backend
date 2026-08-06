35ae615631f56cfc04a94973debef3483941d63f
Bo QIU <35757521+boqiu@users.noreply.github.com>
2026-08-06 17:25:42 +0800

    Optional create auth and gas tank service (#13)

--- api/route.go
+++ api/route.go
@@ -26,17 +26,17 @@ type Config struct {
 func MustServe(config Config, services service.Services) {
 	api.MustServe(config.Config, func(router *gin.Engine) {
 		// set default rate limit config if not configured
-		config.RateLimit.Add(middleware.RateLimitTierFree, "overall", 10, 20)
+		config.RateLimit.Add(middleware.RateLimitTierFree, "overall", 5, 20)
 		// EIP-7702 tx
 		config.RateLimit.Add(middleware.RateLimitTierFree, "setAuth", 1, 5)
 		// gas tank
 		config.RateLimit.Add(middleware.RateLimitTierFree, "signUserOp", 1, 5)
 		// token pay
-		config.RateLimit.Add(middleware.RateLimitTierFree, "getPrice", 5, 10)
+		config.RateLimit.Add(middleware.RateLimitTierFree, "getPrice", 1, 5)
 		config.RateLimit.Add(middleware.RateLimitTierFree, "sponsor", 1, 5)
 
 		// rate limit by client real IP address
-		rateLimiters := middleware.NewRateLimitManager(config.RateLimit, middleware.DefaultRateLimitKeyExtractor)
+		rateLimiters := middleware.NewRateLimitManager(config.RateLimit)
 		go rateLimiters.ScheduleExpire()
 		router.Use(rateLimiters.Middleware("overall"))
 
@@ -48,23 +48,26 @@ func MustServe(config Config, services service.Services) {
 
 		api := router.Group("api")
 
-		aaController := NewAccountAbstractController(services)
-		gasTankController := NewGasTankController(services)
-		tokenPayController := NewTokenPayController(services)
-
 		// test client IP address
 		api.GET("/ip", middleware.Wrap(testClientIP))
 
 		// account abstract - auth
-		api.POST("/aa/auth", rateLimiters.Middleware("setAuth"), middleware.Metrics("api.aa.auth.send"), middleware.Wrap(aaController.SendAuth))
-		api.GET("/aa/auth/:txHash", middleware.Metrics("api.aa.auth.status"), middleware.Wrap(aaController.GetAuthStatus))
+		if services.AccountAbstract != nil {
+			aaController := NewAccountAbstractController(services)
+			api.POST("/aa/auth", rateLimiters.Middleware("setAuth"), middleware.Metrics("api.aa.auth.send"), middleware.Wrap(aaController.SendAuth))
+			api.GET("/aa/auth/:txHash", middleware.Metrics("api.aa.auth.status"), middleware.Wrap(aaController.GetAuthStatus))
+		}
 
 		// Gas tank
-		api.POST("/aa/gastank/prepare/credit", middleware.Metrics("api.aa.gastank.prepare.credit"), middleware.Wrap(gasTankController.PrepareCredit))
-		api.POST("/aa/gastank/prepare/refund", middleware.Metrics("api.aa.gastank.prepare.refund"), middleware.Wrap(gasTankController.PrepareRefund))
-		api.POST("/aa/gastank/sign", rateLimiters.Middleware("signUserOp"), middleware.Metrics("api.aa.gastank.signature"), middleware.Wrap(gasTankController.Sign))
+		if services.GasTank != nil {
+			gasTankController := NewGasTankController(services)
+			api.POST("/aa/gastank/prepare/credit", middleware.Metrics("api.aa.gastank.prepare.credit"), middleware.Wrap(gasTankController.PrepareCredit))
+			api.POST("/aa/gastank/prepare/refund", middleware.Metrics("api.aa.gastank.prepare.refund"), middleware.Wrap(gasTankController.PrepareRefund))
+			api.POST("/aa/gastank/sign", rateLimiters.Middleware("signUserOp"), middleware.Metrics("api.aa.gastank.signature"), middleware.Wrap(gasTankController.Sign))
+		}
 
 		// token pay
+		tokenPayController := NewTokenPayController(services)
 		api.GET("/tokenpay/config", middleware.Metrics("api.tokenpay.config"), middleware.Wrap(tokenPayController.Config))
 		api.GET("/tokenpay/price", rateLimiters.Middleware("getPrice"), middleware.Metrics("api.tokenpay.price"), middleware.Wrap(tokenPayController.GetETHPrice))
 		api.POST("/tokenpay/submit", rateLimiters.Middleware("sponsor"), middleware.Metrics("api.tokenpay.submit"), middleware.Wrap(tokenPayController.Submit))
