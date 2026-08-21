f249ccdb8c4060f85f52db0499f4b93f0b371f44
boqiu <82121246@qq.com>
2026-08-21 18:31:59 +0800

    Delete expired user ops periodically

--- store/models.go
+++ store/models.go
@@ -21,8 +21,8 @@ type UserOp struct {
 	UserOpHash            string    `gorm:"size:66;not null;unique"`
 	Sender                string    `gorm:"size:42;not null;index:idx_sender_status"`
 	Nonce                 string    `gorm:"size:66;not null"`
-	ValidUntil            time.Time `gorm:"not null"`
-	Status                string    `gorm:"size:32;not null;index:idx_sender_status"`
+	ValidUntil            time.Time `gorm:"not null;index:idx_status_valid_until,priority:2"`
+	Status                string    `gorm:"size:32;not null;index:idx_sender_status;index:idx_status_valid_until,priority:1"`
 	ActualGasCost         string    `gorm:"size:32;not null"`
 	ActualUserOpFeePerGas string    `gorm:"size:32;not null"`
 }
