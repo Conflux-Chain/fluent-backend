1f7247c37d4f2f76ef2bd17f6a7ac7f254fa30b3
Bo QIU <35757521+boqiu@users.noreply.github.com>
2026-09-18 10:59:38 +0800

    Pre-init contract ABIs (#46)
    * Pre-init contract ABIs
    
    * Fix copilot comment

--- contract/types.go
+++ contract/types.go
@@ -1,5 +1,30 @@
 package contract
 
-import "github.com/Conflux-Chain/go-conflux-util/blockchain/contract/account"
+import (
+	"strings"
+
+	"github.com/Conflux-Chain/go-conflux-util/blockchain/contract/account"
+	"github.com/Conflux-Chain/go-conflux-util/blockchain/contract/token/erc20"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
 
 type PackedUserOperation = account.PackedUserOperation
+
+var (
+	ERC20ABI        = mustInitABI(erc20.ContractMetaData.ABI)
+	EntryPointABI   = mustInitABI(account.EntryPointMetaData.ABI)
+	SmartAccountABI = mustInitABI(SimpleSmartAccount7702MetaData.ABI)
+)
+
+// mustInitABI initializes an ABI from its JSON representation.
+// It panics if the ABI cannot be parsed.
+//
+// It should not panic, because the ABI JSON is auto-generated via abigen tool.
+func mustInitABI(ABI string) abi.ABI {
+	result, err := abi.JSON(strings.NewReader(ABI))
+	if err != nil {
+		panic(err)
+	}
+
+	return result
+}
